971d8412cdcec82c8194d0728ab0032e55da08c3
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-31 19:55:55 -0400

    Started processing Petition Descriptions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #We the people adlibs py
 import urllib2
 import simplejson as json
+import nltk
 
 #dataResponse = urllib2.urlopen('https://api.whitehouse.gov/v1/petitions.json?limit=300&offset=0')
 #dataInfo = dataResponse.info()
@@ -10,5 +11,11 @@ import simplejson as json
 with open ("petitions.json", "r") as myfile:
     dataJson=myfile.read().replace('\n', '')
 data = json.loads(dataJson)
-
+data = data['results'][2]['body']
 print json.dumps(data, sort_keys=True, indent=4 * ' ')
+print data
+testDescription = data
+
+textData = nltk.word_tokenize(testDescription)
+textTags = nltk.pos_tag(textData)
+print textTags
