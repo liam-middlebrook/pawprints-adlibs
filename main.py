1d697b7e358462d31b7ca14f82ce15a9ff43906d
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-31 20:58:14 -0400

    Now querying for multiple words

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ with open ("petitions.json", "r") as myfile:
     dataJson=myfile.read().replace('\n', '')
 data = json.loads(dataJson)
 data = data['results'][random.randint(0, len(data['results']))]['body']
-print json.dumps(data, sort_keys=True, indent=4 * ' ')
-print data
+#print json.dumps(data, sort_keys=True, indent=4 * ' ')
+#print data
 description = data
 
 #tokenize the description into a list of words
@@ -27,19 +27,22 @@ textTags = nltk.pos_tag(textData)
 #remove duplicates from the tag list
 manTags = list(set(textTags))
 
-#get a random tag from the tagList
-randTag =  manTags[random.randint(0,len(manTags))]
-
-print randTag
-
-#Get a list of all the indices at which a word/tag pair occurs
-replacementIndices = [i for i, x in enumerate(textTags) if x == randTag]
-
-print replacementIndices
-
-
-print "Please type in the replword: "
-replWord = raw_input()
-#Now lets replace each occurance of that word with Butts!
-outputString = description.replace(randTag[0], replWord)
+outputString = description
+for index in range(5):
+    #get a random tag from the tagList
+    randTag =  manTags[random.randint(0,len(manTags))]
+    
+    print randTag
+    
+    #Get a list of all the indices at which a word/tag pair occurs
+    replacementIndices = [i for i, x in enumerate(textTags) if x == randTag]
+    
+    print replacementIndices
+    
+    
+    print "Please type in the replword: "
+    replWord = raw_input()
+    
+    #Now lets replace each occurance of that word with Butts!
+    outputString = outputString.replace(randTag[0], replWord)
 print outputString
