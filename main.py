02034a95b1624a78f5e4b9c979c7adac96e1fc12
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-31 20:52:24 -0400

    Now replaces a random word with a chosen word by user

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import urllib2
 import simplejson as json
 import nltk
+from collections import Counter
 
+import random
 #dataResponse = urllib2.urlopen('https://api.whitehouse.gov/v1/petitions.json?limit=300&offset=0')
 #dataInfo = dataResponse.info()
 #dataJson = dataResponse.read()
@@ -11,11 +13,33 @@ import nltk
 with open ("petitions.json", "r") as myfile:
     dataJson=myfile.read().replace('\n', '')
 data = json.loads(dataJson)
-data = data['results'][2]['body']
+data = data['results'][random.randint(0, len(data['results']))]['body']
 print json.dumps(data, sort_keys=True, indent=4 * ' ')
 print data
-testDescription = data
+description = data
 
-textData = nltk.word_tokenize(testDescription)
+#tokenize the description into a list of words
+textData = nltk.word_tokenize(description)
+
+#pair the words with their tags (adjective, verb, noun, etc.)
 textTags = nltk.pos_tag(textData)
-print textTags
+
+#remove duplicates from the tag list
+manTags = list(set(textTags))
+
+#get a random tag from the tagList
+randTag =  manTags[random.randint(0,len(manTags))]
+
+print randTag
+
+#Get a list of all the indices at which a word/tag pair occurs
+replacementIndices = [i for i, x in enumerate(textTags) if x == randTag]
+
+print replacementIndices
+
+
+print "Please type in the replword: "
+replWord = raw_input()
+#Now lets replace each occurance of that word with Butts!
+outputString = description.replace(randTag[0], replWord)
+print outputString
